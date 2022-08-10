940579d5c5a38d8ed85d5429e7892595d85e0f4b
Yorklt <WereratCleric@gmail.com>
2022-08-10 13:46:18 +0900

    First commit

---
+++ GhostWireWeight.py
@@ -0,0 +1,206 @@
+import bpy
+import gpu
+import gpu_extras.batch
+import bgl
+
+# アドオン情報（名前やバージョンなど）
+bl_info = {
+    "name": "Ghost Wire Weight",
+    "author": "Pon Pon Games",
+    "version": (1, 0),
+    "blender": (3, 2, 1),
+    "location": "3D Viewport > Right side of Header",
+    "description": "This shows wireframe with X-Ray on Wight Pain Mode.",
+    "warning": "At this version, this does not support Modifier. Set bound pose to your character when you paint it.",
+    "support": "TESTING",
+    "doc_url": "",
+    "tracker_url": "",
+    "category": "3D View"
+}
+
+g_draw_handle = None
+g_shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+g_batch = None
+g_last_mode = ""
+g_does_draw = False
+
+# Log
+# 0: Warn and Error
+# 1: Debug
+# 2: Debug (Noisy)
+def print_log(text, level):
+    level_max = 0
+    if level <= level_max:
+        print(text)
+
+# Blender起動後の初回のアドオン有効時だけ呼ばれます。
+print_log("Ghost Wire Weight is called", 1)
+
+# 制御オペレータ
+# 描画ルーチンの起動状態をトグルします。UIに追加したボタンから呼ばれます。
+class GhostWireWeight_OT_ModeController(bpy.types.Operator):
+    bl_idname = "ghostwireweight.modecontroller"
+    bl_label = "Ghost Wire Weight"
+
+    def __init__(self):
+        print_log("ModeController.init is called", 1)
+
+    def __del__(self):
+        print_log("ModeController.del is called", 1)
+
+    def execute(self, context):
+        print_log("ModeController.execute is called", 1)
+
+        if bpy.app.timers.is_registered(update_ghost) == False:
+            start_draw()
+        else:
+            stop_draw()
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        print_log("ModeController.invoke is called", 1)
+        return self.execute(context)
+
+# ボタンの追加
+# 3DViewのヘッダの再描画時にBlenderから呼ばれます。
+def add_toggle_button(self, context):
+    if context.active_object != None:
+        if context.active_object.mode == "WEIGHT_PAINT":
+            row = self.layout.row()
+            row.active = False
+            if bpy.app.timers.is_registered(update_ghost) == True:
+                row.active = True
+            label = "Ghost Wire"
+            row.operator(GhostWireWeight_OT_ModeController.bl_idname, text=label)
+
+# モードの監視とバッチの更新
+# タイマーにより定期的に呼ばれます。
+def update_ghost():
+    print_log("update_ghost is called", 2)
+    
+    global g_last_mode
+    global g_does_draw
+    mode = bpy.context.active_object.mode
+    if mode == "WEIGHT_PAINT":
+        g_does_draw = True
+        if g_last_mode != "WEIGHT_PAINT":
+            recreate_batch()
+    else:
+        g_does_draw = False
+    
+    g_last_mode = mode
+    
+    return 0.1
+
+# 描画
+# 3DViewの再描画時に呼ばれます。
+def draw_wire(dummy):
+    print_log("draw_wire is called", 2)
+
+    global g_does_draw
+    global g_batch
+    global g_shader
+    if g_does_draw == True:
+        if g_batch != None and g_shader != None:
+            bgl.glEnable(bgl.GL_BLEND)
+            bgl.glDisable(bgl.GL_DEPTH_TEST)
+            g_shader.bind()
+            g_shader.uniform_float("color", (1.0, 1.0, 1.0, 0.1))
+            g_batch.draw(g_shader)
+            bgl.glDisable(bgl.GL_BLEND)
+            bgl.glEnable(bgl.GL_DEPTH_TEST)
+            
+# バッチの更新
+# アクティブオブジェクトのメッシュの辺を取得します。
+def recreate_batch():
+    print_log("recreate_batch is called.", 1)
+
+    # Tuple in Tuple
+    # ((x, y, z), (x, y, z), (x, y, z), ...)
+    coords = ()
+    # ((i0, i1), (i0, i1), (i0, i1), ...)
+    indices = ()
+
+    if bpy.context.active_object != None:
+        mesh = bpy.context.active_object.data # bpy_types.mesh
+        if mesh != None:
+                
+            for vertex in mesh.vertices: # bpy.types.MeshVertex
+
+                # co is class Vector
+                coTuple = (vertex.co[0], vertex.co[1], vertex.co[2])
+                coords = coords + (coTuple, )
+            
+            for edge in mesh.edges: # bpy_types.MeshEdge
+
+                index0 = edge.vertices[0]
+                index1 = edge.vertices[1]
+
+                indexTuple = (index0, index1)
+                indices = indices + (indexTuple, )
+
+    global g_batch
+    global g_shader
+    g_batch = gpu_extras.batch.batch_for_shader(
+        g_shader, "LINES", {"pos": coords}, indices=indices
+    )
+
+# 描画開始
+# タイマーの登録(update_ghost)と、描画ハンドラの追加(draw_wire)
+def start_draw():
+    print_log("add_update_timer is called.", 1)
+
+    if bpy.app.timers.is_registered(update_ghost) == False:
+        bpy.app.timers.register(update_ghost, first_interval=0.1, persistent=False)
+
+    global g_draw_handle
+    if g_draw_handle == None:
+        g_draw_handle = bpy.types.SpaceView3D.draw_handler_add(
+            draw_wire, (None, ), "WINDOW", "POST_VIEW"
+        )
+
+# 描画終了
+# タイマーの解除(update_ghost)と、描画ハンドラの削除(draw_wire)
+def stop_draw():
+    print_log("unregister_update_timer is called.", 1)
+
+    if bpy.app.timers.is_registered(update_ghost) == True:
+        bpy.app.timers.unregister(update_ghost)
+
+    global g_draw_handle
+    if g_draw_handle != None:
+        bpy.types.SpaceView3D.draw_handler_remove(g_draw_handle, "WINDOW")
+    g_draw_handle = None
+
+# ファイル読み込み時に、確実に初期状態にします。
+# draw_handlerはそのままだとblendファイルをまたいで存在してしまうので、
+# 強制的に削除します。
+@bpy.app.handlers.persistent
+def reset_status_on_load_post(scene):
+    print_log("reset_status_on_load_post is called.", 1)
+    stop_draw()
+
+# アドオンを有効にしたときにBlenderから呼ばれます。
+# このとき、bpy.dataとbpy.contextにアクセス不可。
+def register():
+    print_log("register is called.", 1)
+    bpy.utils.register_class(GhostWireWeight_OT_ModeController)
+    bpy.types.VIEW3D_HT_tool_header.append(add_toggle_button)
+    bpy.app.handlers.load_post.append(reset_status_on_load_post)
+
+# アドオンを無効にしたときにBlenderから呼ばれます。
+# このとき、bpy.dataとbpy.contextにアクセス不可。
+def unregister():
+    print_log("unregister is called", 1)
+    bpy.app.handlers.load_post.remove(reset_status_on_load_post)
+    bpy.types.VIEW3D_HT_tool_header.remove(add_toggle_button)
+    bpy.utils.unregister_class(GhostWireWeight_OT_ModeController)
+    stop_draw()
+
+# Blenderのテキストエディタで呼んだときの処理（デバッグ用）
+if __name__ == "__main__":
+    print_log("Ghost Wire Weight is called from main", 1)
+    register()
+    #unregister()
+
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # GhostWireWeight
-This is a Blender add-on that shows Wireframe with X-Ray on Weight Paint Mode
+This is a Blender add-on that shows wireframe with X-Ray on Weight Paint Mode.
+これはBlenderのアドオンです。ウェイトペイント時にワイヤーフレームを透過表示します。
+
+# Install
+Save GhostWireWeight.py on your local strage and install it on Preferences.
+GhostWireWeight.pyをローカルに保存して、プリファレンスからインストールしてください。
+
+# Usage
+You can see "Ghost Wire" button on 3D Viewport when it is Weight Pain Mode. Press it.
+ウェイトペイントモードで3Dビューポートの右上に"Ghost Wire"ボタンが表示されます。これを押してください。
+
+# Limitation
+This does not support Modifilers so that paint your character with bound pose (T/A-Pose).
+表示にはモディファイアーが適用されません。ポーズ無し（Tポーズ/Aポーズ）で塗ってください。
+
+# Settings
+Your can change color of wireframe. Open the file with your text editor. Set the 4 values of "g_shader.uniform_float("color", (1.0, 1.0, 1.0, 0.1))". Save it with UTF-8. Install it again on Blender.
+ワイヤーの色を変えることができます。まずファイルをテキストエディタで開きます。g_shader.uniform_float("color", (1.0, 1.0, 1.0, 0.1))の4つの数字を書き換えてください。UTF-8で上書き保存してください。Blenderから再度インストールしてください。
+
+
+
